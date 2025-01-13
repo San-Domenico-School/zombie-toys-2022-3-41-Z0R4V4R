1758c061cefa32576eb6587a009ee312687c90cc
Z0R4V4R <znijjar28@sandomenico.org>
2025-01-13 14:27:12 -0800

    character models complete

---
+++ Assets/Scripts/Helpers/CharacterCenter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCenter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        transform.localPosition = Vector3.zero; 
+    }
+
+   
+}
